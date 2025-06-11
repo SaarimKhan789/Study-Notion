620991733ac17d257938239ee60fdbf74aadb099
Saarim Khan <saarimkhan515@gmail.com>
2025-06-11 14:21:54 +0530

    updated index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion-fn9h-evhwuofvs-saarimkhan515-gmailcoms-projects.vercel.app",
 		credentials:true,
 	})
 )
